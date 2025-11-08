19876de437d4a3ee4fc5d46ba35e9989fa369b74
ashlyn-joshy <ashlyntherese@gmail.com>
2025-11-08 22:27:54 +0530

    delete user by id

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -36,3 +36,17 @@ module.exports.getAllUsers = async (req, res) => {
     res.status(500).json({ message: "Error in fetching users", error: error.message });
   }
 };
+
+//delete user 
+module.exports.deleteUser = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const deletedUser = await User.findByIdAndDelete(userId);
+        if (!deletedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({ message: "User deleted successfully", user: deletedUser });
+    } catch (error) {
+        res.status(500).json({ message: "Error in deleting user", error: error.message });
+    }
+}
--- backend/routers/user.js
+++ backend/routers/user.js
@@ -6,5 +6,6 @@ const userController = require('../controllers/user');
 
 router.post('/createUser', userController.createUser);
 router.get('/getAllUsers', userController.getAllUsers);
+router.delete("/deleteUser/:userId", userController.deleteUser);
 
 module.exports = router;
