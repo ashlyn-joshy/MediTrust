a61d8b28cdda56df6ad6f27af1a1bca766e10ae6
ashlyn-joshy <ashlyntherese@gmail.com>
2025-11-08 18:51:50 +0530

    add user model

---
+++ backend/models/user.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["patient", "doctor", "admin"],
+    default: "patient",
+    required: true,
+  },
+  // only for doctors
+  specialization: {
+    type: String,
+    default: null,
+  },
+  phone: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
