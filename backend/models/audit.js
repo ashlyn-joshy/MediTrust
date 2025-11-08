0e661e577b521a752854cd69fbc81afc5474d41b
ashlyn-joshy <ashlyntherese@gmail.com>
2025-11-08 19:41:52 +0530

    add audit model

---
+++ backend/models/audit.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { act } = require("react");
+const { Schema } = mongoose;
+
+const auditSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  action: { type: String, required: true },
+  entityType: { type: String, required: true },
+  entityId: { type: Schema.Types.ObjectId },
+  timestamp: { type: Date, default: Date.now },
+  change: { type: Object },
+});
+
+module.exports = mongoose.model("Audit", auditSchema);
